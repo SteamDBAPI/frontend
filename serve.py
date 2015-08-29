809d9e263a02e1aebe873ea3739deafb495809b0
PegasusPrime <riverrat87@gmail.com>
2015-08-28 22:01:43 -0400

    Update serve script with actual data

--- serve.py
+++ serve.py
@@ -1,27 +1,47 @@
 #!/usr/bin/python3
 
 from flask import Flask, jsonify, abort, make_response
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.orm import sessionmaker, mapper
+from sqlalchemy.orm.exc import NoResultFound
 
 #Globals
-engine = create_engine('sqlite:///games.db')
 app = Flask(__name__, static_url_path="")
 game_list = []
 
+class Games(object):
+    pass
+
+def loadSession():
+    engine = create_engine('sqlite:///games.db')
+    metadata = MetaData(engine)
+    games_db = Table('games', metadata, autoload=True)
+    mapper(Games, games_db)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
 def build_list(session):
     global game_list
-    result = session.execute('SELECT * FROM games')
+    result = session.query(Games).all()
     for game in result:
-        game_list.append(game.id)
+        game_list.append(str(game.id) + ":" + game.name)
+    session.close()
 
 @app.route('/api/v1/list', methods=['GET'])
 def list():
-    global game_list
     return jsonify({'games': game_list})
 
+@app.route('/api/v1/<gameid>', methods=['GET'])
+def game_dump(gameid):
+    try:
+        result = session.query(Games).filter_by(id=gameid).one()
+    except NoResultFound:
+        return "No results found for that ID"
+    session.close()
+    return jsonify({'name': result.name, 'lowest_price': result.lowest_price})
+
 if __name__ == '__main__':
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session = loadSession()
     build_list(session)
     app.run(host="0.0.0.0", port=8080, debug=True)
